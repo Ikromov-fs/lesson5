3a2550b15195f241b64dd5fac2920af9629de141
Ikromov-fs <nodirikromov10@gmail.com>
2023-04-15 16:03:44 +0300

    new

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,92 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <!-- <router-view /> -->
+  <div>
+    <div>
+      <h1>Ikromov age: {{ $store.state.age }}</h1>
+      <button class="btn btn-success mx-4" @click="$store.state.age += 1">+</button>
+      <button class="btn btn-danger mx-4" @click="$store.state.age -= 1">-</button>
+    </div>
+    <div class="col-md-11 mx-auto mt-4">
+      <div class="col-md-3 my-3">
+        <select class="form-control" @change="selects">
+          <option value="" hidden> change limit</option>
+          <option value="2">2</option>
+          <option value="4">4</option>
+          <option value="6">6</option>
+          <option value="8">8</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+      <table class="table table-hover border">
+        <thead>
+          <tr>
+            <th class="border">T/r</th>
+            <th class="border">email</th>
+            <th class="border">body</th>
+            <th class="border">name</th>
+            <th class="border">postId</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item, index in $store.state.datas" :key="index">
+            <td class="border">{{ item.id }}</td>
+            <td class="border">{{ item.email }}</td>
+            <td class="border">{{ item.body }}</td>
+            <td class="border">{{ item.name }}</td>
+            <td class="border">{{ item.postId }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="text-center">
+      <button class="btn btn-danger mx-4" @click="prev">prev</button>
+      <span class="fs-4">{{ $store.state.count }}</span>
+      <button class="btn btn-success mx-4" @click="next">next</button>
+    </div>
+
+    <div class="text-center mt-5">
+      <div class="mb-3">
+        <p class="fs-5"> {{$store.state.yurish }} Metr</p>
+        <button class="btn btn-warning" @click="yurish">Yurish</button>
+      </div>
+      <div>
+        <p class="fs-5">Qadam kattaligi {{ $store.state.qadamKa }}</p>
+        <button class="btn btn-info" @click="qadam">Qadamni qo'shish</button>
+      </div>
+    </div>
+  </div>
 </template>
+<script setup>
+import store from '@/store';
+import { onMounted } from 'vue';
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+onMounted(() => {
+  store.dispatch('todo')
+})
+const prev = () => {
+  if (store.state.count > 1) {
+    store.dispatch('prev')
+    store.dispatch('todo')
   }
 }
-</style>
+const next = () => {
+  store.dispatch('next')
+  store.dispatch('todo')
+}
+const selects = (e) => {
+  const val = e.target.value
+  store.dispatch(`select`, { val })
+  store.dispatch('todo')
+}
+
+
+const qadam = () => {
+  store.dispatch('qadam')
+}
+
+const yurish = () => {
+  store.dispatch('yurish')
+}
+</script>
+
+<style scoped></style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,61 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    name: 'Nodirbek',
+    age: 19,
+    datas: [],
+    count: 1,
+    limit: 1,
+    qadamKa: 0,
+    yurish: 0,
   },
   getters: {
   },
   mutations: {
+    functons(state, item) {
+      state.datas = item
+    },
+    puls(state, icr) {
+      state.count -= icr
+    },
+    next(state, icr) {
+      state.count += icr
+    },
+    shagat(state, icr) {
+      state.qadamKa += icr
+    },
+    limit(state, val) {
+      state.limit = val
+    },
+    yur(state) {
+      state.yurish += state.qadamKa
+    }
   },
   actions: {
+    todo({ commit, state }) {
+      axios.get(`https://jsonplaceholder.typicode.com/comments?_page=${state.count}&_limit=${state.limit}`).then(res => {
+        const rezult = res.data.splice(0, 100)
+        commit('functons', rezult)
+      })
+    },
+
+    prev({ commit }) {
+      commit('puls', 1)
+    },
+    next({ commit }) {
+      commit('next', 1)
+    },
+    select({ commit }, { val }) {
+      commit('limit', val)
+    },
+    qadam({ commit }) {
+      commit('shagat', 1)
+    },
+    yurish({ commit }) {
+      commit('yur')
+    }
   },
   modules: {
   }
